50bdba2db97905b9547dc728d1bdbf753b6fcfb1
tancredoneves <tancredofernandoneves@gmail.com>
2016-07-21 15:44:09 -0300

    Update Tancredo.java

--- Tancredo.java
+++ Tancredo.java
@@ -1,6 +1,16 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World !");
+import java.util.Scanner;
+import Java.io.*;
+public class CompareStrings
+{
+    public static void main(String[] args)
+    {
+        string s1, s2;
+        Scanner in = new Scanner(System.in)
+        
+        System.out.println("Entre com o primeiro !");
+        s1 = in.nexLine();
+        
+        System.out.println("Entre com o segundo !");
+        s2 = in.nexLine();
     }
 }
