06903e775e45f5f4ad059bb7041210cfcd6b9e15
tancredoneves <tancredofernandoneves@gmail.com>
2016-07-21 16:14:54 -0300

    Update Tancredo.java

--- Tancredo.java
+++ Tancredo.java
@@ -13,7 +13,7 @@ public class CompareStrings
         System.out.println("Entre com o segundo !");
         s2 = in.nexLine();
         
-        if ( se.compareTo(s2) >0 )
+        if ( s1.compareTo(s2) >0 )
             System.out.println("primera maior.");
         else if (s1.compareTo(s2) < 0)   
             System.out.println("segunda maior.");
